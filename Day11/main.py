b15fcf7bbd539ae4111675d1dc2d9b6fa0cc9baa
Coaler <leeyoung.lhg.090407@gmail.com>
2025-12-23 11:35:48 +0700

    Day 11: Custom module and import example

---
+++ Day11/main.py
@@ -0,0 +1,9 @@
+import mymath   
+
+x = float(input("Nhập số thứ nhất: "))
+y = float(input("Nhập số thứ hai: "))
+
+print("Cộng:", mymath.add(x, y))
+print("Trừ:", mymath.subtract(x, y))
+print("Nhân:", mymath.multiply(x, y))
+print("Chia:", mymath.divide(x, y))
