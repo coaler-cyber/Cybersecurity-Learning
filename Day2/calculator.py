2625d13bb283393f03a3810ed4e0ef1df66f47c9
Coaler <leeyoung.lhg.090407@gmail.com>
2025-12-23 10:41:14 +0700

    Day 2: Simple calculator program

---
+++ Day2/calculator.py
@@ -0,0 +1,7 @@
+a = float(input("Nhap so thu nhat: ")
+b = float(input("Nhap so thu hai: ")
+
+print("Ket qua cong:", a + b)
+print("Ket qua tru:", a - b)
+print("Ket qua nhan:", a * b)
+print("Ket qua chia:", a / b)
