5f67186003237dbe01fc715ea43663ce8283d160
Coaler <leeyoung.lhg.090407@gmail.com>
2025-12-23 11:15:40 +0700

    Day 4: For and while loops

---
+++ Day4/loops.py
@@ -0,0 +1,10 @@
+fruits = ["apple", "banana", "cherry"]
+print("Danh sach trai cay:")
+for fruit in fruits:
+	print("-", fruit)
+
+count = 1
+print("\Dem tu 1 den 5:")
+while count <= 5:
+	print(count)
+	count +=1
